cdfa538944fab70eb49931af5742cc7e7a97a0fb
Kat006 <43860472+Kat006@users.noreply.github.com>
2019-04-07 18:57:16 -0400

    7/4

--- Jeu.js
+++ Jeu.js
@@ -47,6 +47,17 @@ function main() {
 	var couleurCarte = (window.location.href).substr((window.location.href)
 			.lastIndexOf("=") + 1);
 
+	function Joeur() {
+		this.tableauDeCartes = [];
+		this.points = 0;
+		this.indexCartes = function() {
+			return this.tableauDeCartes.lenth;
+		};
+	}
+
+	var utilisateur = new Joeur();
+	var userBanque = new Joeur();
+
 	function Cartes(cle, valeur) {
 		this.cle = cle;
 		this.image = new Image();
@@ -64,14 +75,11 @@ function main() {
 			new Cartes("valet.jpg", 10), new Cartes("dame.jpg", 10),
 			new Cartes("roi.jpg", 10) ];
 
-	
-	setTimeout(function(){
+	setTimeout(function() {
 		CartesBanque();
 		MaCarte();
-	},50);
-	
-	
-	
+	}, 50);
+
 	var contexteBanque;
 	function CartesBanque() {
 
@@ -80,10 +88,11 @@ function main() {
 		contexteBanque = boiteBanque.getContext('2d');
 		var imageBanque = LesCartes[indexBanque].image;
 		contexteBanque.drawImage(imageBanque, 45, 20, 165, 220);
-
+		userBanque.points = userBanque.points + LesCartes[indexBanque].valeur;
+		userBanque.tableauDeCartes[userBanque.indexCartes] = LesCartes[indexBanque];
+		document.getElementById("pointsBanque").innerHTML = "La banque a : " + userBanque.points + " points";
 	}
-	
-	
+
 	var contexteMes;
 	function MaCarte() {
 
@@ -92,28 +101,57 @@ function main() {
 		contexteMes = boiteMesCartes.getContext('2d');
 		var image = LesCartes[index].image;
 		contexteMes.drawImage(image, 45, 20, 165, 220);
-
+		utilisateur.points = utilisateur.points + LesCartes[index].valeur;
+		utilisateur.tableauDeCartes[utilisateur.indexCartes] = LesCartes[index];
+		document.getElementById("pointsUser").innerHTML = "Vouz avez : " + utilisateur.points + " points";
 	}
 
-	
-	
-	
-	
-	
-	
+	function Joeur() {
+		this.tableauDeCartes = [];
+		this.points = 0;
+		this.indexCartes = function() {
+			return this.tableauDeCartes.lenth;
+		};
+	}
+
 	var arreter = document.getElementById("arrete");
 	arreter.onclick = function clear() {
 		clearInterval(interval);
+
+		for (var i = 0; i < 4; i++) {
+			CartesBanque();
+		}
+		setTimeout(
+				function() {
+
+					if (userBanque.points > utilisateur.points
+							&& userBanque <= 21) {
+						alert("La banque a gagne avec " + userBanque.points
+								+ "points!")
+					} else if (userBanque.points > 21
+							&& utilisateur.points > 21) {
+						alert("Match null!")
+					} else
+						alert("Vous avez gagne avec " + utilisateur.points
+								+ "points!")
+				}, 150);
+
 	}
 
 	var rejouer = document.getElementById("rejouer");
 	rejouer.onclick = function recetTimer() {
 		clearInterval(interval);
 		chrono();
+		userBanque.points = 0;
+		utilisateur.points = 0;
+		userBanque.tableauDeCartes = [];
+		utilisateur.tableauDeCartes = [];
+		CartesBanque();
+		MaCarte();
 	}
 
 	var newCarte = document.getElementById("newCarte");
-	newCarte.addEventListener("click", function(){
+	newCarte.addEventListener("click", function() {
 		MaCarte();
 	});
 
